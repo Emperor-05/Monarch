db957b27a38ec06327bc4faac6c000863a0108cb
Ritheesh <ritheeshreddy.rr@gmail.com>
2024-11-14 16:22:17 +0530

    Descent of the Monarch

---
+++ src/main/java/in/org/emperor/Monarch/App.java
@@ -0,0 +1,10 @@
+package in.org.emperor.Monarch;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Ritheesh 22BD1A0555");
+    }
+}
